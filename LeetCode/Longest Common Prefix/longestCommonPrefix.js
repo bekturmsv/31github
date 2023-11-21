3d2262ab100e6350866738151bac22165855cd7c
bekturmsv <itsbektur@gmail.com>
2023-11-21 23:30:31 +0600

    started participating in the 31 Days of Coding challenge

---
+++ LeetCode/Longest Common Prefix/longestCommonPrefix.js
@@ -0,0 +1,23 @@
+let longestCommonPrefix = function (strs) {
+  if (strs.length === 0) {
+    return "";
+  }
+
+  strs.sort();
+
+  const firstStr = strs[0];
+  const lastStr = strs[strs.length - 1];
+
+  let commonPrefix = "";
+  for (let i = 0; i < firstStr.length; i++) {
+    if (firstStr.charAt(i) === lastStr.charAt(i)) {
+      commonPrefix += firstStr.charAt(i);
+    } else {
+      break;
+    }
+  }
+
+  return commonPrefix;
+};
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
---
+++ README.md
@@ -0,0 +1,7 @@
+# Started participating in the 31 coding challenge
+
+# My progress 1 / 31
+
+## #ghlchallenge #31github
+
+## @growthhungry, @dtaalai
