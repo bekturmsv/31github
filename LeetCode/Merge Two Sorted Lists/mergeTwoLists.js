ed1a09e620afe470a5774bf9d1e36c0a046a30b2
bekturmsv <itsbektur@gmail.com>
2023-11-23 01:37:13 +0600

    new task from LeetCode, 3/31

---
+++ LeetCode/Merge Two Sorted Lists/mergeTwoLists.js
@@ -0,0 +1,38 @@
+class ListNode {
+  constructor(val, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+function mergeTwoLists(list1, list2) {
+  let dummy = new ListNode(-1);
+  let current = dummy;
+
+  while (list1 !== null && list2 !== null) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+
+  if (list1 !== null) {
+    current.next = list1;
+  }
+
+  if (list2 !== null) {
+    current.next = list2;
+  }
+
+  return dummy.next;
+}
+
+const list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+const list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+const mergedList = mergeTwoLists(list1, list2);
+console.log(mergedList);
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 <span style="font-size: 2rem;"> Started participating in the 31 coding challenge</span>
 
-<span style="font-size: 2rem;">My progress 2 / 31</span>
+<span style="font-size: 2rem;">My progress 3 / 31</span>
