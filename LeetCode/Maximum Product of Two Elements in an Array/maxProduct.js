dde17c67d968b12272399196923f2ff70576844c
bekturmsv <itsbektur@gmail.com>
2023-12-13 02:31:23 +0600

    add 'maxProduct' task, day 23 of 31

---
+++ LeetCode/Maximum Product of Two Elements in an Array/maxProduct.js
@@ -0,0 +1,8 @@
+const maxProduct = (nums) => {
+  nums.sort((a, b) => b - a);
+  return (nums[0] - 1) * (nums[1] - 1);
+};
+
+const nums = [3, 4, 5, 2];
+const result = maxProduct(nums);
+console.log(result);
